0111f2ce505ad3640df62aed829891a073503db4
Leonardo Romero <lrm_93@hotmail.com>
2023-05-14 23:15:55 -0600

    End of day 2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 // Variables
 const ageCalcInputsMax2 = document.querySelectorAll(".age-calc__input.max-2");
 const ageCalcInputsMax4 = document.querySelectorAll(".age-calc__input.max-4");
+const allInputs = document.querySelectorAll(".age-calc__input");
+const allInputsTitles = document.querySelectorAll(".age-calc__h1");
+const btnAgeCalc = document.querySelector(".age-calc__btn");
 // .
 // .
 // .
@@ -8,7 +11,7 @@ const ageCalcInputsMax4 = document.querySelectorAll(".age-calc__input.max-4");
 // .
 // .
 // .
-// . On inputs
+// . Constraining numbers in the inputs
 ageCalcInputsMax2.forEach((input) => {
   input.setAttribute(
     "oninput",
@@ -21,3 +24,35 @@ ageCalcInputsMax4.forEach((input) => {
     "this.value = this.value.replace(/[^0-9]/g, '').substring(0, 4);"
   );
 });
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// . Functions
+function calculateAge() {
+  for (let index in allInputs) {
+    let containWarning = false;
+    containWarning = allInputs[index].classList.contains("warning");
+
+    if (allInputs[index].value === "") {
+      allInputsTitles[index].classList.add("warning");
+      allInputs[index].classList.add("warning");
+    } else if (containWarning === true) {
+      allInputsTitles[index].classList.remove("warning");
+      allInputs[index].classList.remove("warning");
+      console.log(`removio ${index}`);
+    }
+  }
+}
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// . Event listener
+btnAgeCalc.addEventListener("click", calculateAge);
