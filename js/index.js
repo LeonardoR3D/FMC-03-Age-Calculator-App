1a25a7f69f0b73f74ae4ad344005dda7528d9640
Leonardo Romero <lrm_93@hotmail.com>
2023-05-15 21:06:43 -0600

    Day 3

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
-// Variables
-const ageCalcInputsMax2 = document.querySelectorAll(".age-calc__input.max-2");
-const ageCalcInputsMax4 = document.querySelectorAll(".age-calc__input.max-4");
-const allInputs = document.querySelectorAll(".age-calc__input");
-const allInputsTitles = document.querySelectorAll(".age-calc__h1");
-const btnAgeCalc = document.querySelector(".age-calc__btn");
+// Global variables
+const inputDaySection = document.querySelector("#input-day-section");
+const inputMonthSection = document.querySelector("#input-month-section");
+const inputYearSection = document.querySelector("#input-year-section");
+const inputDayTitle = document.querySelector("#input-day-title");
+const inputMonthTitle = document.querySelector("#input-month-title");
+const inputYearTitle = document.querySelector("#input-year-title");
+const inputDay = document.querySelector("#input-day");
+const inputMonth = document.querySelector("#input-month");
+const inputYear = document.querySelector("#input-year");
+
+const allInputsSections = [
+  inputDaySection,
+  inputMonthSection,
+  inputYearSection,
+];
+const allInputsTitles = [inputDayTitle, inputMonthTitle, inputYearTitle];
+const allInputs = [inputDay, inputMonth, inputYear];
+
+const btnAgeCalc = document.querySelector("#btn-age-calc");
 // .
 // .
 // .
@@ -12,18 +26,17 @@ const btnAgeCalc = document.querySelector(".age-calc__btn");
 // .
 // .
 // . Constraining numbers in the inputs
-ageCalcInputsMax2.forEach((input) => {
+[inputDay, inputMonth].forEach((input) => {
   input.setAttribute(
     "oninput",
     "this.value = this.value.replace(/[^0-9]/g, '').substring(0, 2);"
   );
 });
-ageCalcInputsMax4.forEach((input) => {
-  input.setAttribute(
-    "oninput",
-    "this.value = this.value.replace(/[^0-9]/g, '').substring(0, 4);"
-  );
-});
+
+inputYear.setAttribute(
+  "oninput",
+  "this.value = this.value.replace(/[^0-9]/g, '').substring(0, 4);"
+);
 // .
 // .
 // .
@@ -32,20 +45,66 @@ ageCalcInputsMax4.forEach((input) => {
 // .
 // .
 // . Functions
-function calculateAge() {
-  for (let index in allInputs) {
-    let containWarning = false;
-    containWarning = allInputs[index].classList.contains("warning");
-
-    if (allInputs[index].value === "") {
-      allInputsTitles[index].classList.add("warning");
-      allInputs[index].classList.add("warning");
-    } else if (containWarning === true) {
+function clearWarnings(array) {
+  array.forEach((input, index) => {
+    if (input.classList.contains("warning")) {
+      let selectElementP = allInputsSections[index].querySelector(
+        ".age-calc__warning-text"
+      );
       allInputsTitles[index].classList.remove("warning");
-      allInputs[index].classList.remove("warning");
-      console.log(`removio ${index}`);
+      input.classList.remove("warning");
+      allInputsSections[index].removeChild(selectElementP);
     }
-  }
+  });
+}
+
+function checkIfIsEmpty(array) {
+  array.forEach((input, index) => {
+    let warningText = document.createElement("p");
+    warningText.setAttribute("class", "age-calc__warning-text");
+    warningText.textContent = "This field is required";
+
+    if (input.value === "") {
+      allInputsTitles[index].classList.add("warning");
+      input.classList.add("warning");
+      allInputsSections[index].appendChild(warningText);
+    }
+  });
+}
+
+function checkIfIsValid(array) {
+  array.forEach((input, index) => {
+    if (!input.classList.contains("warning")) {
+      let warningText = document.createElement("p");
+      warningText.setAttribute("class", "age-calc__warning-text");
+      warningMessage = [
+        "Must be a valid day",
+        "Must be a valid month",
+        "Must be in the past",
+      ];
+      let value = ["day", "month", "year"];
+      // Change the 2023 condition later for the current year
+      let condition = [31, 12, 2023];
+
+      if (Number(input.value) > condition[index]) {
+        allInputsTitles[index].classList.add("warning");
+        input.classList.add("warning");
+        warningText.textContent = warningMessage[index];
+        allInputsSections[index].appendChild(warningText);
+      } else if (Number(input.value) === 0) {
+        allInputsTitles[index].classList.add("warning");
+        input.classList.add("warning");
+        warningText.textContent = `The ${value[index]} can't be 0`;
+        allInputsSections[index].appendChild(warningText);
+      }
+    }
+  });
+}
+
+function calculateAge() {
+  clearWarnings(allInputs);
+  checkIfIsEmpty(allInputs);
+  checkIfIsValid(allInputs);
 }
 // .
 // .
